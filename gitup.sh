fc9c6c09b258b2798e384376168e01ff71846924
nachoba <0800nacho@gmail.com>
2017-07-15 18:48:29 -0300

    gitter

--- gitup.sh
+++ gitup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+
+# Add recently changed files
 git add *
 
+# Commit with message provided by the user
 git commit -m "$1"
 
+# Push
 git push -u origin master
+
+
+# For better use copy this file:
+# sudo cp gitup.sh /usr/bin/gitup
 
